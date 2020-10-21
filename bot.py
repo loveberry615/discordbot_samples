8283eac5e4000603d64b9ea6273cf7608b58250b
loveberry615 <73095845+loveberry615@users.noreply.github.com>
2020-10-21 17:09:08 +0900

    update token id

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 #discordのライブラリをインポート
 import discord
 #自分のトークンにしてね
-TOKEN = 'THi5IsDuMMyaCCesSTOK3nQ4.Cl2FMQ.ThIsi5DUMMyAcc3s5ToKen7kKWs'
+TOKEN = 'NzY3OTkxMDU0MTI0MzE4NzUw.X459Zw.-u9AiLWqWCjd-Qk3hFdYMUC_8qo'
 
 # 接続に必要なオブジェクトを作る
 client = discord.Client()
